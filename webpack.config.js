2a79b4eb926e704d156fbaa76c2255a2477a4d3c
peripheralvisions <85910344+peripheralvisions@users.noreply.github.com>
2022-02-10 14:24:36 +0100

    Adding meta viewport for scaling

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,9 @@ const config = {
   plugins: [
     new HtmlWebpackPlugin({
       templateContent: ({ htmlWebpackPlugin }) =>
-        '<!DOCTYPE html><html><head><meta charset="utf-8"><title>Bootmine Note App</title></head><body><div id="app"></div></body></html>',
+        `<!DOCTYPE html><html><head><meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+        <title>Bootmine Note App</title></head><body><div id="app"></div></body></html>`,
       filename: "index.html",
     }),
   ],
